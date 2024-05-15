819663eea95713274c258c391b05affb132a33f0
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-05-14 21:35:57 -0300

    Queries e Resolvers

---
+++ GraphQLDemo.API/Program.cs
@@ -0,0 +1,14 @@
+using GraphQLDemo.API.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>();
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
